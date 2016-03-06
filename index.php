27ab8f36a644798394fce0f5247a09f6a8a2a242
yerson <omar@gmail.com>
2016-03-06 01:36:17 -0500

    primer proyecto

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estilos2.css">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+	
+</head>
+<body>
+<div class="contenedor">
+	<nav>
+		<ul class="menu-lista">
+			<li class="menu-item"><a href ="" class="menu-link">INICIO</a></li>
+			<li class="menu-item"><a href ="" class="menu-link">NOSOTROS</a></li>
+			<li class="menu-item"><a href ="" class="menu-link">USTEDES</a></li>
+			<li class="menu-item"><a href ="" class="menu-link">CONTACTENOS</a></li>
+			
+		</ul>
+	</nav>
+	<div class="slider">
+		<img src="diptico_beca_18-01.jpg" height="350" width="960">
+		<
+
+	</div>
+</body>
+</html>
+
+<img src="1-141212161623455.jpg" height="100" width="180">
+<img src="image.jpg" height="100" width="180">
+	<img src="diptico_beca_18-01.jpg" height="100" width="180">
+	<img src="diptico_beca_18-01.jpg" height="100" width="180">
+	<img src="1-141212161623455.jpg" height="100" width="180">
+
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title></title>
+		<link rel="stylesheet" href="">
+	</head>
+	<body>
+
+		
+<h2>NO SUPIMOS CONECTAR</h2>
+		<h2>La historia que voy a contar comenzó en marzo 2013, cuando tuvimos 14 años. Después de un año siendo buenos amigos, que hablabamos entre los cambios de clase compartiendo nuestras opiniones, le fui cogiendo poco a poco cariño y más cariño (con una gran protección que la tuve). Terminé en una clase de informática a séptima hora de un miércoles dia 27 enamorandome de ella.</h2>
+		<img src="imagenes-gif-animados-de-lindo-paisaje.gif" height="500" width="940">
+		<<embed  src="escalera al cielo bo go ship da HD.mp3" height="14" width="14">
+        <<video autobuffer autoloop loop controls height="600"width="600">
+        	<source src="♪ MÁS ALLÁ DEL CIELO - YO TE VOY A AMAR.mp4">
+        	<object type="video/ogg" data="/media/video.oga" width="0" height="0">
+        	<param name="src" value="/media/video.oga">
+        	<param name="autoplay" value="false">
+        	<param name="autoStart" value="1">
+        	<p><a href="/media/video.oga">Download this video file.</a></p>
+        	</object>
+        </video>
+        	
+        </header><!-- /header -->>
+
+		<h3> NO ES UNA HOSTORI TRISTE</h3>
+
+		<h3> Por aquel entonces estaba sola, en mi casa mi madre trabajaba todos los días y muchas noches, mi padre no nos prestaba atención, cuando mis padres se juntaban solo discutían y mi hermano los calmaba y también me calmaba a mí que no podía dejar de llorar, pero se fue de casa y no supe otra vez de él. </h3>
+		<img src="yerson.gif" height="500" width="940">
+
+
+
+	</body>
+	</html>
+	
